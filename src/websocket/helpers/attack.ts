d866fcf323b57db4871a8a1e6f55c008ec7f392c
VikaLubenets <lubenetcvika@gmail.com>
2024-10-28 19:59:38 +0100

    fix: fix status killed for ship

--- src/websocket/helpers/attack.ts
+++ src/websocket/helpers/attack.ts
@@ -25,28 +25,38 @@ export function attack(message: AttackRequest): { response: AttackResponse | nul
         status = "shot";
         opponentStatus.matrix[y][x] = -1;
 
-        const arr = Object.values(opponentStatus.ships);
-
-        const isKilled = arr.some(ship => {
+        const arr = Object.values(opponentStatus.ships)
+    
+        const ship = arr.find(ship => {
             const positions = Array.from({ length: ship.length }, (_, i) => ({
                 x: ship.direction ? ship.position.x : ship.position.x + i,
                 y: ship.direction ? ship.position.y + i : ship.position.y,
             }));
-            
-            return positions.every(pos => opponentStatus.matrix[pos.y][pos.x] === -1);
+    
+            return positions.some(pos => pos.x === x && pos.y === y);
         });
-
-        if (isKilled) {
-            status = "killed";
-            data.deleteShip(game.id, opponentStatus.indexPlayer)
-
-            const updShips = data.getGameById(game.id)?.gameStatus.find((status) => status.indexPlayer === opponentStatus.indexPlayer)?.remainingShips
-            if(updShips === 0){
-                winnerId = currentPlayerId
-                gameWon = true
+    
+        if (ship) {
+            const isKilled = Array.from({ length: ship.length }, (_, i) => ({
+                x: ship.direction ? ship.position.x : ship.position.x + i,
+                y: ship.direction ? ship.position.y + i : ship.position.y,
+            })).every(pos => opponentStatus.matrix[pos.y][pos.x] === -1);
+    
+            if (isKilled) {
+                status = "killed";
+                data.deleteShip(game.id, opponentStatus.indexPlayer);
+    
+                const remainingShips = data.getGameById(game.id)
+                    ?.gameStatus.find(status => status.indexPlayer === opponentStatus.indexPlayer)?.remainingShips;
+                    
+                if (remainingShips === 0) {
+                    winnerId = currentPlayerId;
+                    gameWon = true;
+                }
             }
         }
     }
+    
 
     response = {
         type: "attack",
